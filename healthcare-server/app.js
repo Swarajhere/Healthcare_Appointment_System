a8c79f592e51e3fb6bf4f7e99450b9a5b99b3172
Swarajhere <swaraj.nalawade2004@gmail.com>
2025-06-21 23:37:44 +0530

    route

--- healthcare-server/app.js
+++ healthcare-server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const userRoutes = require('./routes/userRoutes');
 const adminRoutes = require('./routes/adminRoutes'); // New admin routes
 const appointmentRoutes = require("./routes/appointmentRoutes");
+const auth = require('./routes/auth'); // Import auth routes
 const app = express();
 const cors = require('cors');
 
@@ -10,5 +11,6 @@ app.use(express.json()); // Middleware to parse JSON
 app.use('/api', userRoutes); // Mount user routes
 app.use('/api/admin', adminRoutes); // Mount admin routes
 app.use("/api", appointmentRoutes);
+app.use('/api/auth', auth); // Mount auth routes
 
 module.exports = app;
